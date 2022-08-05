fd7f9f9b80cb86c96b9d70ad8516db08cda2d602
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-06 01:54:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,6 @@ Bot = Client(
     api_id=API_ID,
     api_hash=API_HASH
 )
-
-@Bot.on_message(filters.private)
-async def hagadmansa(bot, message):
-  if message.text:
-      return 
-  hagadmansa = await message.reply("`Processing...`")
-  media = message.photo or message.video or message.voice or message.document or message.animation or message.audio or message.sticker or message.VideoNote
-  link = f"https://t.me/{BOT_USERNAME}?start={media.file_id}"
-  share = f"https://t.me/share/url?url={link}&text=Click%20on%20link%20to%20get%20the%20file%20now,%20Join%20@Hagadmansa"
-  await hagadmansa.edit(
-    text=f"Here is your link: {link}",
-    reply_markup=InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('Share now', url=share)
-        ]])
-  )
 @Bot.on_message(filters.command('start'))
 async def start(bot, message):
     if len(message.command) == 1:
@@ -57,9 +41,21 @@ async def start(bot, message):
             )
         except:
             await message.reply('The media you are trying to send is invalid.')
-
-@Bot.on_message(filters.command('hello'))
-async def start(bot, message):
-    await message.reply('hello')
+            
+@Bot.on_message(filters.private)
+async def hagadmansa(bot, message):
+  if message.text:
+      return 
+  hagadmansa = await message.reply("`Processing...`")
+  media = message.photo or message.video or message.voice or message.document or message.animation or message.audio or message.sticker or message.VideoNote
+  link = f"https://t.me/{BOT_USERNAME}?start={media.file_id}"
+  share = f"https://t.me/share/url?url={link}&text=Click%20on%20link%20to%20get%20the%20file%20now,%20Join%20@Hagadmansa"
+  await hagadmansa.edit(
+    text=f"Here is your link: {link}",
+    reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('Share now', url=share)
+        ]])
+  )
     
 Bot.run()

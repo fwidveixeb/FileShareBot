bee66bdcc2c53cf8d78e4e037deb0b650e715a5c
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-06 01:48:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ async def start(bot, message):
         except:
             await message.reply('The media you are trying to send is invalid.')
 
-Bot.on_message(filters.command('hello'))
+@Bot.on_message(filters.command('hello'))
 async def start(bot, message):
     await message.reply('hello')
     
